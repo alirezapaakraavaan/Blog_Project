e67e8923968f3e4c4808028e3b51a76f36201804
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-07-22 21:37:08 +0430

    Admin Theme Implemented

---
+++ Blog/ServiceHost/Areas/Administration/Pages/Blog/ArticleCategories/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ServiceHost.Areas.Administration.Pages.Blog.ArticleCategories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
