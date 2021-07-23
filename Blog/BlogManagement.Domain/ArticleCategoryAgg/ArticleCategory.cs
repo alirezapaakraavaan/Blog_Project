d05bf0e75efc1f11413620ff59f3ecf35e42e8c2
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-07-23 20:21:53 +0430

    Domain & Application Of ArticleCategory Implemented

---
+++ Blog/BlogManagement.Domain/ArticleCategoryAgg/ArticleCategory.cs
@@ -0,0 +1,45 @@
+﻿using _0_Framework.Domain;
+
+namespace BlogManagement.Domain.ArticleCategoryAgg
+{
+    public class ArticleCategory : EntityBase
+    {
+        public string Name { get; private set; }
+        public string Picture { get; private set; }
+        public string Description { get; private set; }
+        public int ShowOrder { get; private set; }
+        public string Slug { get; private set; }
+        public string Keywords { get; private set; }
+        public string MetaDescription { get; private set; }
+        public string CanonicalAddress { get; private set; }
+
+        public ArticleCategory(string canonicalAddress, string description, string keywords, string metaDescription,
+            string name, string picture, int showOrder, string slug)
+        {
+            CanonicalAddress = canonicalAddress;
+            Description = description;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Name = name;
+            Picture = picture;
+            ShowOrder = showOrder;
+            Slug = slug;
+        }
+
+        public void Edit(string canonicalAddress, string description, string keywords, string metaDescription,
+            string name, string picture, int showOrder, string slug)
+        {
+            CanonicalAddress = canonicalAddress;
+            Description = description;
+            Keywords = keywords;
+            MetaDescription = metaDescription;
+            Name = name;
+
+            if (!string.IsNullOrWhiteSpace(picture))
+                Picture = picture;
+
+            ShowOrder = showOrder;
+            Slug = slug;
+        }
+    }
+}
--- Blog/ServiceHost/Startup.cs
+++ Blog/ServiceHost/Startup.cs
@@ -1,3 +1,4 @@
+using _0_Framework.Application;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -15,13 +16,12 @@ namespace ServiceHost
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<IFileUploader, FileUploader>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -31,7 +31,6 @@ namespace ServiceHost
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
